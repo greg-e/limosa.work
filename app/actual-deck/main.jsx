051cd5f701f26debb11cf83000170ee85270335d
Greg Ehrenberg <greg@ehrenberg.us>
2025-11-02 20:32:07 -0500

    Update main.jsx

--- app/actual-deck/main.jsx
+++ app/actual-deck/main.jsx
@@ -1,1 +1,7 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import ActualDeckApp from "./ActualDeckApp.jsx";
+import "./styles.css";
+
+createRoot(document.getElementById("actual-deck")).render(<ActualDeckApp />);
 
