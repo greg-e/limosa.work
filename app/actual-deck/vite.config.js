31d805e8eb1803b7d4e2aa14944ca013dc6c72d3
Greg Ehrenberg <greg@ehrenberg.us>
2025-11-02 20:31:19 -0500

    Update vite.config.js

--- app/actual-deck/vite.config.js
+++ app/actual-deck/vite.config.js
@@ -1,1 +1,18 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: "app/actual-deck",
+  build: {
+    outDir: "../../assets/actual-deck",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: "app/actual-deck/main.jsx",
+      output: {
+        entryFileNames: "main.js",   // stable filename
+        assetFileNames: "[name].[ext]"
+      }
+    }
+  },
+  base: "/"
+});
 
