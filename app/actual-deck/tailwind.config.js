189ae05cad15e11c901298c4bf65e1e680bbd7d1
Greg Ehrenberg <greg@ehrenberg.us>
2025-11-02 20:52:44 -0500

    Create tailwind.config.js

---
+++ app/actual-deck/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app/actual-deck/**/*.{html,js,jsx,ts,tsx}",
+    "./_layouts/**/*.{html,md}",
+    "./_includes/**/*.{html,md}",
+    "./**/*.md",
+    "./**/*.html"
+  ],
+  theme: { extend: {} },
+  plugins: []
+};
